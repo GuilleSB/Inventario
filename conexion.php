95898cf23eb8ce0c4b1e3fd2813085569eb1b473
Guillermo Sotomayor <guillesotoblanco@gmail.com>
2020-05-03 20:18:24 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,9 +1,9 @@
 <?php
-$hostname= "localhost";
-$database = "3406236_guatusodev";
-$userbd= "root";
-$password = "";
-$port= "3308";
+$hostname= "#";
+$database = "#";
+$userbd= "#";
+$password = "#";
+$port= "#";
 
 $mysql = new mysqli($hostname, $userbd, $password, $database, $port);
 
